aee239b708b7cb9e9e6b9a26c75df59d46f5787a
AnnNavarrete <AnnNavarrete@users.noreply.github.com>
2019-02-05 08:01:37 +0000

    20190205T080118Z

---
+++ vigenere.c
@@ -0,0 +1,64 @@
+// Ann Navarrete
+// 05 February 2019
+// Problem Set 2 - Vigenere
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int shift(char c);
+
+int main(int argc, string argv[])
+{
+    // Ensure the program has only one command-line
+    if (argc != 2)
+    {
+        printf("Usage: ./vigenere key\n");
+        return 1;
+    }
+    else
+    { 
+        // Ensure user only enter key in alphabet
+        string key = (argv[1]);
+        
+        //Make sure command line argument is alpha
+        for (int i = 0; i < strlen(key); i++)
+        {
+            if (isalpha(key[i]) == false)
+            {
+                printf("Usage: ./vigenere key\n");
+                return 1;
+            }
+        } 
+        // Prompt user input in plaintext
+        string plaintext = get_string("plaintext: ");
+        printf("ciphertext: ");
+        
+        for (int i = 0; i < strlen(plaintext); i++)
+        {   
+            int keyChar = key[i % strlen(key)];
+            int shift = shift(keyChar);
+            printf("%c", plaintext[i] + shift);
+        }
+        printf("\n");
+    }   
+    return 0;
+}
+
+int shift(char c)
+{
+    // If isUpper then minus 65
+    if (isupper(c))
+    {
+        return c - 65;
+    }
+    
+    // If isLower then minus 97
+    else if (islower(c))
+    {
+        return c - 97;
+    }
+    return 0;
+}
+
